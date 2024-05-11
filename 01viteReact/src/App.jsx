15b24c8947598d9d46da183254f3f83b11a353cc
pratikgondkar <pratikgondkar4040@gmail.com>
2024-05-11 16:46:51 +0100

    basic react

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  const username = 'chai aur react'  // { } yala mantat expresion evaluvate expression likh shakate
+
+  return (
+    <>
+      <h1>pratik Gondkar {username}</h1>  
+    </>
+  )
+}
+
+export default App
